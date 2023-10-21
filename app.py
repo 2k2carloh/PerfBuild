0d58bd3883badeea3b9cdd0802bcca51e3927233
Carlos Rivera <138614508+2k2carloh@users.noreply.github.com>
2023-10-21 20:06:15 -0300

    Alfa 0.2
    Segundo commit jeje

--- app.py
+++ app.py
@@ -14,14 +14,16 @@ def mostrar_menu():
     print("║ 1. ║Agregar Usuarios")
     print("║ 2. ║Ver Trabajadores")
     print("║ 3. ║Editar Trabajadores")
+    print("║ 4. ║Agrear Tareas")
+    print("║ 5. ║Ver Tareas")
     print("║ 0. ║Salir")
     print("=================================")
-    print('Dev by: Carlos Rivera, Cristobal Morales, Catalina Bulnes')
-
+    print('Proyecto de: Carlos Rivera, Cristobal Morales, Catalina Bulnes')
 
 worker_name = []
 worker_years = []
 worker_level = []
+activity_faena = {}
 
 # Enter workers
 def joinWorkers ():
@@ -35,14 +37,14 @@ def joinWorkers ():
         years = int(input("Ingresa los años del trabajador: "))
         worker_years.append(years)
 
-        level = int(input("Ingrese el nivel del trabajador.\n1.- Poca experiencia (Junior - 1-3 años)\n2.- Mediana experiencia (Mid - 3-5 años)\n3.- Bastante experiencia (Senior - 5 o más años)\n"))
+        level = int(input("Ingrese el nivel del trabajador.\n1.- Poca experiencia (Junior - 1-3 años)\n2.- Mediana experiencia (Mid - 3-6 años)\n3.- Bastante experiencia (Senior - 7 o más años)\n"))
 
         if level == 1:
-            worker_level.append("Junior")
+            worker_level.append("Junior (Junior - 1-3 años)")
         elif level == 2:
-            worker_level.append("Mid")
+            worker_level.append("Mid (Mid - 3-6 años)")
         elif level == 3:
-            worker_level.append("Senior")
+            worker_level.append("Senior (Senior - 7 o más años)")
         else: 
             worker_level.append("No definido")
 
@@ -56,7 +58,7 @@ def viewWorker ():
         search_profile = int(input("Ingrese el ID de la persona que desea buscar: "))
 
         if search_profile == 0:
-            print("El perfil no existe")
+            print("Error: El perfil no existe")
         elif search_profile <= len(worker_name):
             print("=================================")
             print(f'║ Usuario {search_profile} de {len(worker_name)} ')
@@ -67,7 +69,7 @@ def viewWorker ():
             print(f"║Experiencia: ║ {worker_level[search_profile - 1]}") 
             print("=================================")
         else:
-            print("El perfil no existe")
+            print("Error: El perfil no existe")
 
         break_while = int(input("Para buscar otro ID coloque 1, para salir, 0.\n"))
 
@@ -82,10 +84,81 @@ def getInfo():
     days = float(input("Cuantos dias de trabajo realizara?: "))
     total_days = days * meters
 
+def modiftyWorker():
+    
+    while True:
+
+        search_profile = int(input("Ingresa el ID del trabajador que desea modificar: "))
+
+        if search_profile == 0:
+            print("El perfil no existe")
+        elif search_profile <= len(worker_name):
+            print("=================================")
+            print(f'║ Usuario {search_profile} de {len(worker_name)} ')
+            print("=================================")
+            print(f"║ID:          ║ {search_profile}")
+            print(f"║Nombre:      ║ {worker_name[search_profile - 1]}")
+            print(f"║Edad:        ║ {worker_years[search_profile - 1]}")
+            print(f"║Experiencia: ║ {worker_level[search_profile - 1]}") 
+            print("=================================")
+            print('')
+            print("=======================")
+            print(f"║0. ║ Salir          ║")
+            print(f"║1. ║ Nombre         ║")
+            print(f"║2. ║ Edad           ║")
+            print(f"║3. ║ Experiencia    ║") 
+            print("=======================")
+
+            while True: 
+                modify_profile = int(input("¿Que esea modificar este perfil?: "))
+
+                if modify_profile == 1:
+                    new_name = input('Ingresa el nuevo nombre: ')
+                    worker_name[search_profile - 1] = new_name
+                elif modify_profile == 2:
+                    new_years = int(input('Ingresa la nueva edad: '))
+                    worker_years[search_profile - 1] = new_years
+                elif modify_profile == 3:
+                    new_level = int(input("Ingresa el nuevo nivel de experiencia: \n1.- Poca experiencia (Junior - 1-3 años)\n2.- Mediana experiencia (Mid - 3-6 años)\n3.- Bastante experiencia (Senior - 7 o más años)\n"))
+                    if new_level == 1:
+                        worker_level[search_profile - 1] = "Junior (Junior - 1-3 años)"
+                    elif new_level == 2:
+                        worker_level[search_profile - 1] = "Mid (Mid - 3-6 años)"
+                    elif new_level == 3:
+                        worker_level[search_profile - 1] = "Senior (Senior - 7 o más años)"
+                    else: 
+                        worker_level.append("No definido")
+                else:
+                    print("Error: Opción no valida")
+
+                modify_profile = int(input('Desea modificar otra información?\n1. Si \n0. No\n'))
+                if modify_profile == 0:
+                    break
+
+        else:
+            print("El perfil no existe")
+
+        break_while = int(input("Para modificar otro perfil coloque 1, para salir, 0.\n"))
+
+        if break_while == 0:
+            clear_console()
+            break  
+
+        clear_console()
+
+# Restrictions
+def restrictions ():
+    
+    lista_de_restricciones = {
+        "Mal clima": None,
+        "Equipamiento": None,
+        "Contrato": None,
+        "Dificultad": None,
+        "Supervision": None
+    }
 
 # Request faena activity
 def faena():
-    activity_faena = {}
 
     while True:
         activity = input("¿Ingrese la actividad que desea realizar?: ")
@@ -102,19 +175,24 @@ def faena():
 
         clear_console()
 
-    print( activity_faena )
+# View Faerna Activity
+def viewFaena():
+    print("=================================")
+    print("║ Tarea ║ Repeticiones")
+    print("=================================")
+    for key, value in activity_faena.items():
+        print(f"║ {key} - {value}")
 
-# Restrictions
-def restrictions ():
-    
-    lista_de_restricciones = {
-        "Mal clima": None,
-        "Equipamiento": None,
-        "Contrato": None,
-        "Dificultad": None,
-        "Supervision": None
-    }
+    print("=================================")
+    print('')
+    ghost_input = input("Para continuar presione una tecla")
 
+    if ghost_input == "a":
+        clear_console()
+    else:
+        clear_console()
+
+# MENU NAVEGATION
 while True:
     mostrar_menu()
     opcion = input("Ingrese el número de opción que desea ejecutar: ")
@@ -126,7 +204,14 @@ while True:
         clear_console()
         viewWorker()
     elif opcion == "3":
-        print('hola') 
+        clear_console()
+        modiftyWorker()
+    elif opcion == "4":
+        clear_console()
+        faena()
+    elif opcion == "5":
+        clear_console()
+        viewFaena()
     elif opcion == "0":
         clear_console()
         print("Saliendo del programa...")
